f56997c32292be4fbe3f3040bf882c4736bc06b7
Josue Eliezer Gomez Soto <jsegomez06@gmail.com>
2024-10-15 15:52:48 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+import { CuscaOtpComponent } from 'cusca-otp';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'validation-otp';
+  private dialog = inject(MatDialog);
+
+  openDialog():void{
+    const dialog = this.dialog.open(CuscaOtpComponent,{
+      autoFocus: false
+    });
+  }
 }
